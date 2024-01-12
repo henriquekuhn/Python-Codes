87d092ff08c3a3abfa3c55c809c1819dadd233f8
Henrique Kuhn <henriqueckuhn@gmail.com>
2024-01-12 11:38:00 -0300

    update

--- Higher Lower Game/Teste.py
+++ Higher Lower Game/Teste.py
@@ -1,5 +1,22 @@
 #Find a specific LIST index
 
-for index, my_dict in enumerate(game_data):
-        if my_dict == dict_exclude:
-            del game_data[index]
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    }
+]
+
+print(data)
+for index, my_dict in enumerate(data):
+        if my_dict == data[0]:
+            del data[index]
+print(data)
