f2e930fbc32c4ad754ab340fe4836c55073ab9a8
adm_cafrunikuhn <henriqueckuhn@gmail.com>
2024-02-05 16:34:04 -0300

    Pong Game finished

--- Pong Game/paddle.py
+++ Pong Game/paddle.py
@@ -1,23 +1,23 @@
-
+from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, paddle_position):
         super().__init__()
         self.shape("square")
         self.shapesize(5, 1)
         self.color("white")
         self.penup()
-        self.goto(x_position, y_position)
+        self.goto(paddle_position)
 
-    def go_up():
-        if paddle.ycor() < 250:
-            new_y = paddle.ycor() + 20
-            paddle.goto(paddle.xcor(), new_y)    
+    def go_up(self):
+        if self.ycor() < 300:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)    
 
-    def go_down():
-        if paddle.ycor() > -250:
-            new_y = paddle.ycor() - 20
-            paddle.goto(paddle.xcor(), new_y) 
+    def go_down(self):
+        if self.ycor() > -300:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y) 
 
     
