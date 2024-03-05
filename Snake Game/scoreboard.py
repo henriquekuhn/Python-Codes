c54f726012e8555dc30b25a6d21528b79a861e56
adm_cafrunikuhn <henriqueckuhn@gmail.com>
2024-03-05 14:38:46 -0300

    Snake Game: adding txt file management to save high score

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -1,19 +1,24 @@
 from turtle import Turtle
+import os 
 
 ALIGNMENT = "center"
-FONT = ("Arial", 24, "normal")
+FONT = ("Arial", 18, "normal")
+DIRECTORY = os.getcwd()+"/Snake Game"
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        os.chdir(DIRECTORY)
+        with open("score.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(0, 260)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT) 
+        #self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT) 
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1 
@@ -28,9 +33,13 @@ class Scoreboard(Turtle):
         if self.score > self.high_score:
             self.high_score = self.score
         self.score = 0
+        self.save()
         self.update_scoreboard()
 
-
+    def save(self):
+        with open("score.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
+        
 
     #def game_over(self):
     #    self.goto(0, 0)
