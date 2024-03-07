029031922160f112defa7c7a1f17ff41901de8d3
adm_cafrunikuhn <henriqueckuhn@gmail.com>
2024-03-07 10:26:40 -0300

    Automatic name replace letter update

--- Automating Mail Recipients/main.py
+++ Automating Mail Recipients/main.py
@@ -1,12 +1,14 @@
 import os
 
-DIRECTORY = os.getcwd() + "/Automating Mail Recipients"
-os.chdir(DIRECTORY)
+DIRECTORY_IN = os.getcwd() + "/Automating Mail Recipients/Input"
+DIRECTORY_OUT = os.getcwd() + "/Automating Mail Recipients/Output"
+os.chdir(DIRECTORY_IN)
 
 class recipient_mail:
 
     def __init__(self):
         self.name_list = []
+        self.common_letter = []
 
     # Use the class variable directly, without 'self.'
     def get_recipients(self):
@@ -15,16 +17,26 @@ class recipient_mail:
                 self.name_list.append(line.strip())
         print(self.name_list)
 
-    def save_mail(self):
+
+    def get_common_letter(self):
+        with open("common_letter.txt") as file:
+            self.common_letter = file.read()
+            print(self.common_letter)
+
+
+    def update_mail(self):
+        os.chdir(DIRECTORY_OUT)
+        
         for name in self.name_list:
+            self.new_letter = self.common_letter.replace("Someone", name)
+            print(f"{self.new_letter}")
             with open(f"letter_for_{name}.txt", "w") as file:
-                file.write(f"Dear {name},\n\n")
-                file.write("I am chalanging you to a cheese run.\n\n")
-                file.write("Hope you can face it!\n\n")
-                file.write("Chuck Norris.")
+                file.write(self.new_letter)
+                
             
                 
 
 write_mail = recipient_mail()
 write_mail.get_recipients()
-write_mail.save_mail()
+write_mail.get_common_letter()
+write_mail.update_mail()
